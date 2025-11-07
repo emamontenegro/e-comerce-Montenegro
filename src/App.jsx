b4e62977fa52cf5b2e16f93978a78210c2bc5903
emamontenegro <montenegroemanuel995@gmail.com>
2025-11-07 20:50:16 -0300

    CreaTuLanding Montenegro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Home from './pages/home'
 import Header from './components/layout/header'
 import './App.css'
