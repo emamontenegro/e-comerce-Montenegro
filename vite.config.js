6836c0d345754ca32c3cc04bec9fd2208466e4a3
emamontenegro <montenegroemanuel995@gmail.com>
2025-12-26 19:29:18 -0300

    CreaTuLanding Montenegro

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig(() => ({
   plugins: [react()],
-  base: command === "build" && process.env.VERCEL
-    ? "/"
-    : "/e-comerce-Montenegro/",
+  base: process.env.VERCEL ? "/" : "/e-comerce-Montenegro/",
 }));
