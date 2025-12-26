32c79186f013682ce70971f618bbfd0db0bb929b
emamontenegro <montenegroemanuel995@gmail.com>
2025-12-26 20:00:53 -0300

    CreaTuLanding Montenegro

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,7 @@ import App from './App.jsx'
 import { AuthProvider } from "./context/auth/index.jsx";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter  
-  basename={import.meta.env.PROD && !import.meta.env.VERCEL
-    ? "/e-comerce-Montenegro"
-    : "/"}>
+  <BrowserRouter basename="/">
     <AuthProvider>
       <App />
     </AuthProvider>
